da829172c572a0696be46628befa881e97b0066e
Natsu4814 <natsu4814yt@gmail.com>
2022-02-07 16:28:54 +0200

    master

--- main.cpp
+++ main.cpp
@@ -5,36 +5,5 @@ char yn;
 
 int main()
 {
-start:
-    Coffee_Shop shop;
-    Coffee_Shop_Admin s1;
-    Guest g1;
-    cout << "> Are you admin(y/n): ";
-    char yn;
-    cin >> yn;
-    if(yn == 'y'|| yn == 'Y')
-    {
-        if(s1.logIn() == true)
-        {
-            do
-            {
-                cout << "> Enter name: ";
-                string name;
-                getline(cin, name);
-                cout << "> Enter price for medium cup: ";
-                int price_m;
-                cout << "> Enter price for large cup: ";
-                int price_l;
-                s1.add_coffee_type(name, price_m,  price_l);
-                cout << "> Would you liek to continue(y/n): ";
-                cin >> yn;
-            } while(yn == 'y');
-            goto start;
-        }
-    }
-    else
-    {
-        g1.show_menu();
-        shop.make_choise();
-    }
+
 }
