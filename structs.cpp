7c88eedfb6163039c594ba35a135ea2eb3dc67c1
Natsu4814 <89647677+Natsu4814@users.noreply.github.com>
2022-04-27 22:38:22 +0300

    master

--- structs.cpp
+++ structs.cpp
@@ -52,6 +52,11 @@ public:
 		}
 		return -1;
 	}
+	void add_guest(string name, string phone_number, int count)
+	{
+		Guest_BIO g1 = {name, phone_number, count};
+		guests.push_back(g1);
+	}
 	vector<Guest_BIO> get_guests_vector()
 	{
 		return guests;
@@ -94,6 +99,46 @@ public:
 	}
 };
 
+class User_Manager
+{
+	My_String string_to_tok;
+	vector<Guest_BIO> people_buffer;
+public:
+	void load_users(Guests &g1)
+	{
+		string line;
+		string phone_number;
+		string name;
+		int count;
+		ifstream fin;
+		string* strings;
+		fin.open("guests.txt");
+		while(!fin.eof())
+		{
+			getline(fin, line);
+			strings = string_to_tok.tok(line, ' ');
+			name = strings[0];
+			phone_number = strings[1];
+			count = stoi(strings[2]);
+			g1.add_guest(name, phone_number, count);
+		}
+		fin.close();
+	}
+	void save_users(Guests g1)
+	{
+		people_buffer = g1.get_guests_vector();
+		ofstream fout;
+		fout.open("guests.txt");
+			for(int i = 0; i < people_buffer.size(); i++)
+			{
+				fout << people_buffer.at(i).name << " " << people_buffer.at(i).phone_number << " " << people_buffer.at(i).count+1;
+				if(i < people_buffer.size()-1)
+					fout << "\n";
+			}
+		fout.close();
+	}
+};
+
 class Receipt
 {
 	double sum;
@@ -101,26 +146,27 @@ class Receipt
 	vector<Type_Coffee> coffees;
 	vector<Type_Topping> toppings;
 public:
-	void set_guest(string name)
-	{
-		this->name = name;
-	}
 	void choose_coffee(vector<Type_Coffee> c1, int pos)
-	{
+	{	
+		pos -= 1;
 		coffees.push_back(c1.at(pos));
 	}
 	void choose_topping(vector<Type_Topping> t1, int pos, int ammount_of_topping)
 	{
-		for(int i = 0; i < ammount_of_topping; i++)
-			toppings.push_back(t1.at(pos));
+		pos -= 1;
+		if(pos == -1)
+		{
+			return;
+		}
+		else
+		{
+			for(int i = 0; i < ammount_of_topping; i++)
+				toppings.push_back(t1.at(pos));
+		}
 	}
 	int get_ammount_of_coffee()
 	{
 		return coffees.size();
-	}
-	string get_user()
-	{
-		return name;
 	}
 	vector<Type_Coffee> get_coffees()
 	{
@@ -130,16 +176,32 @@ public:
 	{
 		return toppings;
 	}
-	void count_sum()
+	double count_sum(string phone_number)
 	{
+		Guests g1;
+		User_Manager m;
+		m.load_users(g1);
+		vector<Guest_BIO> gsts = g1.get_guests_vector();
+		int a = g1.log_in(phone_number);
 		for(int i = 0; i < coffees.size(); i++)
 			sum += coffees.at(i).price;
-		for(int i = 0; i < toppings.size(); i++)
-			sum += toppings.at(i).price;
+		if(toppings.size() != 0)
+			for(int i = 0; i < toppings.size(); i++)
+				sum += toppings.at(i).price;
+		if(a != -1)
+			if(gsts.at(a).count > 5)
+				return sum * 0.95;
+		else
+			return sum;
+
 	}
-	double get_sum()
+	void set_name(string name)
 	{
-		return sum;
+		this->name = name;
+	}
+	string get_name()
+	{
+		return name;
 	}
 };
 
@@ -148,7 +210,7 @@ class IO_Manager
 	My_String string_to_tok;
 	vector<Type_Coffee> coffee_buffer;
 	vector<Type_Topping> topping_buffer;
-	vector<Guest_BIO> people_buffer;
+	string phone_number;
 public:
 //Menu saving
 	void save_to_file(Coffee_Shop c1)
@@ -202,41 +264,6 @@ public:
 		}
 		fin.close();
 	}
-//User saving
-	void save_users(Guests g1)
-	{
-		people_buffer = g1.get_guests_vector();
-		ofstream fout;
-		fout.open("guests.txt");
-			for(int i = 0; i < people_buffer.size(); i++)
-			{
-				fout << people_buffer.at(i).name << " " << people_buffer.at(i).phone_number << " " << people_buffer.at(i).count;
-				if(i < people_buffer.size()-1)
-					fout << "\n";
-			}
-		fout.close();
-	}
-//User loading	
-	void load_users(Guests &g1)
-	{
-		string line;
-		string phone_number;
-		string name;
-		int count;
-		ifstream fin;
-		string* strings;
-		fin.open("guests.txt");
-		while(!fin.eof())
-		{
-			getline(fin, line);
-			strings = string_to_tok.tok(line, ' ');
-			name = strings[0];
-			phone_number = strings[1];
-			count = stoi(strings[3]);
-			g1.sign_up(name, phone_number);
-		}
-		fin.close();
-	}
 
 	void save_receipt(Receipt r1)
 	{
@@ -244,20 +271,21 @@ public:
 		topping_buffer = r1.get_toppings();
 		ofstream fout;
 		fout.open("receipts.txt", ios_base::app);
-		fout << r1.get_user() << "\n";
+		fout << r1.get_name() << "\n";
 		for(int i = 0; i < coffee_buffer.size(); i++)
 		{
-			fout << coffee_buffer.at(i).coffee_name << " " << coffee_buffer.at(i).price;
-			if(i < coffee_buffer.size()-1)
-				fout << "\n";
+			fout << coffee_buffer.at(i).coffee_name << " " << coffee_buffer.at(i).price << " UAH\n";
 		}
 		for(int i = 0; i < topping_buffer.size(); i++)
 		{
-			fout << topping_buffer.at(i).topping << " " << topping_buffer.at(i).price;
-			if(i < topping_buffer.size()-1)
-				fout << "\n";
+			fout << topping_buffer.at(i).topping << " " << topping_buffer.at(i).price << " UAH\n";
 		}
-		fout << r1.get_sum() << " UAH\n====================";
+		fout << r1.count_sum(phone_number) << " UAH\n====================\n";
+	}
+
+	void set_phone_number(string phone_number)
+	{
+		this->phone_number = phone_number;
 	}
 };
 
@@ -266,6 +294,7 @@ class Menu_Handler
 	Coffee_Shop c1;
 	Guests g1;
 	IO_Manager manager;
+	User_Manager m2;
 	Receipt r1;
 	char yes_no;
 
@@ -318,15 +347,16 @@ public:
 	void menu()
 	{
 		manager.load_into_menu(c1);
-		manager.save_to_file(c1);
-		manager.load_users(g1);
-		cout << "\t\t\t\t\t\t\t\tWelcome\n";
+		manager.save_to_file(c1); 
+		m2.load_users(g1);
+ 		cout << "\t\t\t\t\t\t\t\tWelcome\n";
 		cout << "Do you want to use/create discount card, or just make an order(1/2): ";
 		cin >> switcher;
 		if(switcher == 1)
 		{
 			cout << "> Enter your phone number: ";
 			cin >> phone_number;
+			manager.set_phone_number(phone_number);
 			if(phone_number == "admin")
 			{
 				cin.ignore();
@@ -397,13 +427,40 @@ public:
 						r1.choose_topping(c1.get_topping_types(), position, ammount);
 					}
 				}
-				r1.set_guest(new_guest->name);
+				m2.save_users(g1);
 				manager.save_receipt(r1);
 			}
 		}
 		else if(switcher == 2)
 		{
 			show_coffee_shop_menu();
+			do
+			{
+				std::cout << "Which kind of coffee do you want to order, enter the number of position: ";
+				std::cin >> position;
+				r1.choose_coffee(c1.get_coffee_types(), position);
+				std::cout << "Want to order some more or choose topping(y/n): ";
+				std::cin >> yes_no;
+			} while (yes_no == 'y' || yes_no == 'Y');
+			int size = r1.get_ammount_of_coffee();
+			for(int i = 0; i < size; i++)
+			{
+				std::cout << "Choose topping from menu(0 - nothing): ";
+				std::cin >> position;
+				if(position == 0)
+					break;
+				else if(position != 0 && position <! 0)
+				{
+					std::cout << "Enter ammount of topping";
+					std::cin >> ammount;
+					r1.choose_topping(c1.get_topping_types(), position, ammount);
+				}
+			}
+			cout << "Enter your name please: ";
+			getline(cin, name);
+			r1.set_name(name);
+			m2.save_users(g1);
+			manager.save_receipt(r1);
 		}
 	}
 	Guest_BIO* login_as_guest(Guests g1 ,string phone_number)
@@ -411,6 +468,8 @@ public:
 		vector<Guest_BIO> guest_box = g1.get_guests_vector();
 		int guest_number;
 		guest_number = g1.log_in(phone_number);
+		string name = guest_box.at(guest_number).name;
+		r1.set_name(name);
 		Guest_BIO *ptr;
 		if(guest_number >= 0)
 		{
@@ -456,7 +515,7 @@ public:
 				g1.sign_up(name, phone_number);
 				guest_number = g1.log_in(phone_number);
 				ptr = &guest_box[guest_number];
-				manager.save_users(g1);
+				m2.save_users(g1);
 				return ptr;
 			}		
 		}
